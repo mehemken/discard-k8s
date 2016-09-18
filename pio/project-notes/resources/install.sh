97337b0b1d8259bbd70f7cb818905c64bc6aa8ba
Marco Hemken <mehemken@gmail.com>
2016-09-18 13:00:28 -0700

    modified the install.sh script for docker.

--- pio/project-notes/resources/install.sh
+++ pio/project-notes/resources/install.sh
@@ -1,7 +1,7 @@
 # This is how you install docker on a vm
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install -y apt-transport-https ca-certificates
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
@@ -11,10 +11,10 @@ sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' >> \
 sudo apt-get update
 apt-cache policy docker-engine
 
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get install -y docker-engine
 sudo service docker start
 
 
